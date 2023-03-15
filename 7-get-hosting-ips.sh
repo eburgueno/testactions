fecaaa02ffeaa0e0a0e8030ba14fc66a236c4931
Eric Burgueño <eric.burgueno@sitehost.co.nz>
2023-03-16 10:02:02 +1300

    Hosting IPs v2

--- 7-get-hosting-ips.sh
+++ 7-get-hosting-ips.sh
@@ -5,19 +5,20 @@ mkdir -p "$HOSTING_IPS_DIR"
 
 CONTAINER=$(docker ps -q)
 
-wget -q -O /tmp/hostingRanges.tsv.zip https://github.com/NikolaiT/IP-Address-API/raw/main/databases/hostingRanges.tsv.zip
-cd /tmp
-unzip /tmp/hostingRanges.tsv.zip
-rm -f /tmp/hostingRanges.tsv.zip
-cd -
+wget -q -O hostingRanges.tsv.zip https://github.com/NikolaiT/IP-Address-API/raw/main/databases/hostingRanges.tsv.zip
+#cd /tmp
+unzip hostingRanges.tsv.zip
+rm -f hostingRanges.tsv.zip
+#cd -
 
-#if last field is empty, use a trimmed version of the first one
-#if range is cidr already, add empty start and end fields
-#else, add empty cidr field
-#remove first field
-cat /tmp/hostingRanges.tsv | awk -F'\t' 'BEGIN{OFS="\t"} {if ($NF=="") { gsub(/[^[:alnum:]]/, "", $1); $NF=$1 }} { gsub(/[[:space:]]+/,"",$2) } {if ($2 ~ /\//) { $2="\t\t"$2 } else { gsub(/-/,"\t",$2); $3="\t"$3 }} {for(i=2;i<=NF-1;i++) printf $i"\t"; print $NF}' > /tmp/preprocessed.tsv
+#preprocessing
+##if last field is empty, use a trimmed version of the first one
+##if range is cidr already, add empty start and end fields
+##else, add empty cidr field
+##remove first field
+cat hostingRanges.tsv | awk -F'\t' 'BEGIN{OFS="\t"} {if ($NF=="") { gsub(/[^[:alnum:]]/, "", $1); $NF=$1 }} { gsub(/[[:space:]]+/,"",$2) } {if ($2 ~ /\//) { $2="\t\t"$2 } else { gsub(/-/,"\t",$2); $3="\t"$3 }} {for(i=2;i<=NF-1;i++) printf $i"\t"; print $NF}' > preprocessed.tsv
 
-docker cp /tmp/preprocessed.tsv "${CONTAINER}":/tmp/preprocessed.tsv
+docker cp preprocessed.tsv "${CONTAINER}":/tmp/preprocessed.tsv
 docker exec -u postgres "${CONTAINER}" mkdir /tmp/pgexport
 
 export PGPASSWORD="postgres"
@@ -25,6 +26,7 @@ psql -h localhost -p 5432 -U postgres ipdata -c "CREATE TABLE hosting(ipstart IN
 
 psql -h localhost -p 5432 -U postgres ipdata -c "COPY hosting FROM '/tmp/preprocessed.tsv' WITH (DELIMITER E'\t', NULL '');"
 
+psql -h localhost -p 5432 -U postgres ipdata -c "UPDATE hosting SET iprange=inet_merge(ipstart, ipend) WHERE iprange IS NULL;"
 psql -h localhost -p 5432 -U postgres ipdata <<'EOF'
 DO $$
 DECLARE
@@ -33,7 +35,7 @@ filename text;
 BEGIN
 FOR rec IN SELECT DISTINCT provider FROM hosting LOOP
 filename := '/tmp/pgexport/' || rec.provider;
-EXECUTE format('COPY (SELECT CASE WHEN iprange IS NOT NULL THEN iprange ELSE inet_merge(ipstart,ipend) END AS cidr FROM hosting WHERE provider = %L ORDER BY cidr) TO %L', rec.provider, filename);
+EXECUTE format('COPY (SELECT iprange as cidr FROM hosting WHERE provider = %L ORDER BY cidr) TO %L', rec.provider, filename);
 END LOOP;
 END $$;
 EOF
